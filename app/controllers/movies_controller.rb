71cf1d6dee230f74d626d57622bfb96f8760efb8
bestowros <fbestowr@uncc.edu>
2013-03-17 16:27:11 -0700

    first commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,51 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+      @ratings = params[:ratings]
+    @sort_by = params[:sort_by]
+    session_ratings = (@ratings.nil? and !session[:ratings].nil?)
+    session_sort_by = (@sort_by.nil? and !session[:sort_by].nil?)
+    if session_ratings or session_sort_by
+      if session_ratings
+        # no ratings filter selected by user
+        # session exists with ratings filter
+        @ratings = session[:ratings]
+      end
+      if session_sort_by
+        # no table sort specified by user
+        # session exists with table sort
+        @sort_by = session[:sort_by]
+      end
+      redirect_to :action => 'index', :ratings => @ratings, :sort_by => @sort_by
+    end
+    
+    # query database for all movies matching criteria
+    @all_ratings = Movie.all_ratings
+    find_params = Hash.new
+    
+    if @ratings
+      # user requested a rating filter
+      ratings = @ratings.keys
+      if ratings.length > 0
+        find_params[:conditions] = ["rating IN (?)", ratings]
+      end
+      
+      # save the ratings filter in a session
+      session[:ratings] = @ratings
+    else
+      @ratings = Hash.new
+    end
+    
+    if (@sort_by == 'title') or (@sort_by == 'release_date')
+      # user requested a table sort
+      find_params[:order] = @sort_by
+      
+      # save tje table sort in a session
+      session[:sort_by] = @sort_by
+    end
+    
+    @movies = Movie.find(:all, find_params)
+ 
   end
 
   def new
